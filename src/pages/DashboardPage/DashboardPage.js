e2ce473d32d684ba0793d175a622444da1323f97
lucasemdb <lucas@ebi.ac.uk>
2024-03-10 22:34:54 +0000

    Add EbiHeader component to DashboardPage

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import EbiHeader from '../../components/EbiHeader/EbiHeader'; // assuming you have a corresponding EbiHeader React component
 import './DashboardPage.css'; // assuming you have converted the SCSS to CSS
 
 const DashboardPage = () => {
